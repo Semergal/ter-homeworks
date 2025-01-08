316c8aa18da92450d86b7973d7d1c7d874b87145
Konstantin <troshinkv@mail.ru>
2025-01-08 16:02:46 +1000

    6

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,24 +1,24 @@
 resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
+  name = var.cloud.develop.vpc_name
 }
 resource "yandex_vpc_subnet" "develop" {
-  name           = var.vpc_name
-  zone           = var.default_zone
+  name           = var.cloud.develop.vpc_name
+  zone           = var.cloud.develop.default_zone
   network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = var.default_cidr
+  v4_cidr_blocks = var.cloud.develop.default_cidr
 }
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm.web-develop.image
 }
-resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+resource "yandex_compute_instance" "platform-web-develop" {
+  name        = "${local.project}-${local.function}-${local.instance}-web-develop"
+  platform_id = "standard-v1"
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm.web-develop.cores
+    memory        = var.vm.web-develop.memory
+    core_fraction = var.vm.web-develop.core_fraction
   }
   boot_disk {
     initialize_params {
@@ -37,5 +37,30 @@ resource "yandex_compute_instance" "platform" {
     serial-port-enable = 1
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
+}
+resource "yandex_compute_instance" "platform-db" {
+  name        = "${local.project}-${local.function}-${local.instance}-db"
+  platform_id = "standard-v1"
+  resources {
+    cores         = var.vm.db.cores
+    memory        = var.vm.db.memory
+    core_fraction = var.vm.db.core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
 
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
 }
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,13 +1,32 @@
 ###cloud vars
-
+variable cloud {
+  type = map(object({
+    cloud_id = string
+    folder_id = string
+    default_zone = string
+    default_cidr = list(string)
+    vpc_name = string
+  }))
+    default = { 
+      "develop" = {
+        cloud_id = "b1g66n0nchd3o474t6jq"
+        folder_id = "b1g5lr038h98q79traaf"
+        default_zone = "ru-central1-a"
+        default_cidr = ["10.0.1.0/24"]
+        vpc_name = "develop"
+      }
+  }
+}
 
 variable "cloud_id" {
   type        = string
+  default     = "b1g66n0nchd3o474t6jq"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1g5lr038h98q79traaf"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -27,12 +46,42 @@ variable "vpc_name" {
   default     = "develop"
   description = "VPC network & subnet name"
 }
+variable "token"{
+  type        = string
+  description = "enter secret token"
+}
+
+
+variable vm {
+  type = map(object({
+    cores = number
+    memory = number
+    core_fraction = number
+    image = string
+  }))
+    default = { 
+      "web-develop" = {
+        cores = 2
+        memory = 1
+        core_fraction = 5
+        image = "ubuntu-2204-lts"
+      },
+      "db" = {
+        cores = 2
+        memory = 2
+        core_fraction = 20
+        image = "ubuntu-2204-lts"
+        zone = "ru.central1-b"
+      }
+  }
+}
+
 
 
 ###ssh vars
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIL089Y5GGOpFLjqBNjSOe0D1Hiuti6VD5ObRVeiGFg9 root@net1"
   description = "ssh-keygen -t ed25519"
 }
